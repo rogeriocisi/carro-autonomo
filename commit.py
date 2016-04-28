4e57f73c384f429a4c517238242b1662421642f3
rogerio <rogeriocisi@gmail.com>
2016-04-28 16:52:19 -0300

    ./commit.py scripts do Github

--- commit.py
+++ commit.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python
 
+# Este programa faz o add, commit e push no Github
+
 import commands
 import subprocess
 import os
 import sys
+import sys
+
+comentario = ''
+
+for arg in sys.argv:
+    comentario = comentario + ' ' + arg
 
 pr = subprocess.Popen( "/usr/bin/git add *" , cwd = os.path.dirname( '/home/rogerio/carro-autonomo/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
 (out, error) = pr.communicate()
 
-print "Error1 : " + str(error) 
-print "out1 : " + str(out)
+print "Error : " + str(error) 
+print "out : " + str(out)
 
-pr = subprocess.Popen( "/usr/bin/git commit -m 'teste'" , cwd = os.path.dirname( '/home/rogerio/carro-autonomo/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+pr = subprocess.Popen( "/usr/bin/git commit -m '" + comentario + "'", cwd = os.path.dirname( '/home/rogerio/carro-autonomo/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
 (out, error) = pr.communicate()
 
-print "Error2 : " + str(error) 
-print "out2 : " + str(out)
+print "Error : " + str(error) 
+print "out : " + str(out)
 
 pr = subprocess.Popen( "/usr/bin/git push origin master" , cwd = os.path.dirname( '/home/rogerio/carro-autonomo/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
 (out, error) = pr.communicate()
 
-print "Error3 : " + str(error) 
-print "out3 : " + str(out)
+print "Error : " + str(error) 
+print "out : " + str(out)
