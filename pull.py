6d62e300d19fb75dea6c246f8040893051ff209a
rogerio <rogeriocisi@gmail.com>
2016-04-28 17:08:46 -0300

    atualizacao

--- pull.py
+++ pull.py
@@ -5,9 +5,8 @@ import subprocess
 import os
 import sys
 
-pr = subprocess.Popen( "/usr/bin/git pull" , cwd = os.path.dirname( '/home/rogerio/carro-autonomo/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+pr = subprocess.Popen( "/usr/bin/git pull" , cwd = os.path.dirname( os.getcwd() + "/" ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
 (out, error) = pr.communicate()
-
 
 print "Error : " + str(error) 
 print "out : " + str(out)
