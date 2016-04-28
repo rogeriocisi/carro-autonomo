0919906cd73584ecb6389d98e9f1d883ac67b1de
rogerio <rogeriocisi@gmail.com>
2016-04-28 16:53:23 -0300

    ./commit.py scripts do Github

--- commit.py
+++ commit.py
@@ -16,17 +16,15 @@ for arg in sys.argv:
 pr = subprocess.Popen( "/usr/bin/git add *" , cwd = os.path.dirname( '/home/rogerio/carro-autonomo/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
 (out, error) = pr.communicate()
 
-print "Error : " + str(error) 
-print "out : " + str(out)
+print "saida : " + str(error) + "\n" + str(out)
 
 pr = subprocess.Popen( "/usr/bin/git commit -m '" + comentario + "'", cwd = os.path.dirname( '/home/rogerio/carro-autonomo/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
 (out, error) = pr.communicate()
 
-print "Error : " + str(error) 
-print "out : " + str(out)
+print "saida : " + str(error) + "\n" + str(out)
 
 pr = subprocess.Popen( "/usr/bin/git push origin master" , cwd = os.path.dirname( '/home/rogerio/carro-autonomo/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
 (out, error) = pr.communicate()
 
-print "Error : " + str(error) 
-print "out : " + str(out)
+print "saida : " + str(error) + "\n" + str(out)
+
