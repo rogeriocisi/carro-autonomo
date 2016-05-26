1027a808f8dfe9e6d894cbb62b6e38308e20016c
rogeriocisi@gmail.com <rogeriocisi@gmail.com>
2016-05-26 17:19:24 -0300

    atualizacao

---
+++ octave/teste_oct2py.py
@@ -0,0 +1,11 @@
+from oct2py import octave
+
+'''
+import numpy as np
+x = np.array([[1, 2], [3, 4]], dtype=float)
+out, oclass = octave.roundtrip(x)
+import pprint
+pprint.pprint([x, x.dtype, out, oclass, out.dtype])
+''''
+
+
